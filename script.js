d8543f4501ac951a3bb8a938a955b85ca9f84fca
Suman Das <sum4ndas@gmail.com>
2022-01-10 22:18:25 +0530

    ROCK PAPER SCISSORS with DOM v1

--- script.js
+++ script.js
@@ -1,84 +1,85 @@
 // Game of Rock Paper Scissors.
 let computerScore = 0;
 let playerScore = 0;
-let gameRound = 1;
+
+const buttons = document.querySelector('.buttons');
+const choices = document.querySelectorAll("h3");
+const results = document.querySelectorAll(".results > p > span");
+
+buttons.addEventListener('click', (event) => {
+    let computerChoice = getComputerChoice();
+    playRound(event.target.id, computerChoice);
+    choices[0].textContent = `Your choice: ${event.target.id}`;
+    choices[1].textContent = `Computer choice: ${computerChoice}`;
+    printRoundScore();
+    printFinalResult();
+});
 
 // computer selects either rock, paper or scissors.
 function getComputerChoice() {
     let randomIndex = Math.floor(Math.random() * 3);
-    let choices = ["rock", "paper", "scissors"];
-    return choices[randomIndex];
+    let itemChoices = ["rock", "paper", "scissors"];
+    let cpuChoice = itemChoices[randomIndex];
+    console.log(cpuChoice);
+    return cpuChoice;
 }
 
-// Asks user for input.
-function getPlayerChoice() {
-    let playerChoice = prompt("What is your weapon? Rock, Paper or Scissors");
-    return playerChoice.toLowerCase();
-}
 
 // play single round of Rock Paper Scissors.
 function playRound(playerSelection, computerSelection) {
     if (computerSelection == playerSelection) {
-        return "Draw!"
+        console.log("Draw");
+        return choices[2].textContent = "Draw";
     } else {
         if (playerSelection == "rock") {
             if (computerSelection == "paper") {
                 computerScore += 1;
-                return "You lose! Paper beats rock.";
+                console.log("You lose! Paper beats rock.")
+                return choices[2].textContent = "You lose! Paper beats rock.";
             } else {
                 playerScore += 1;
-                return "You win! Rock beats scissors"
+                console.log("You win! Rock beats scissors");
+                return choices[2].textContent = "You win! Rock beats scissors";
             }
         } else if (playerSelection == "paper") {
             if (computerSelection == "scissors") {
                 computerScore += 1;
-                return "You lose! Scissors beats paper."
+                return choices[2].textContent = "You lose! Scissors beats paper."
             } else {
                 playerScore += 1;
-                return "You win! Paper beats rock."
+                return choices[2].textContent = "You win! Paper beats rock."
             }
         } else if (playerSelection == "scissors") {
             if (computerSelection == "rock") {
                 computerScore += 1;
-                return "You lose! Rock beats scissors."
+                return choices[2].textContent = "You lose! Rock beats scissors."
             } else {
                 playerScore += 1;
-                return "You win! Scissors beat paper."
+                return choices[2].textContent = "You win! Scissors beat paper."
             }
         }
     }
 }
 
-
-// Play 5 round game. Report winner or looser at the end.
-function game() {
-    alert("Welcome to Rock Paper Scissors. Choose your weapon wisely!!!")
-    while (gameRound <= 5) {
-        let computerSelection = getComputerChoice();
-        let playerSelection = getPlayerChoice()
-        console.log(`Game round: ${gameRound}`);
-        console.log(`Player: ${playerSelection} Cpu: ${computerSelection}`)
-        console.log(playRound(playerSelection, computerSelection));
-        gameRound += 1;
-        console.log(`Player Score: ${playerScore}, Computer Score: ${computerScore}`);
-        // Prints blank line at console after every round.
-        console.log();
-    }
-
-    printFinalResult();
-    // gameRound = 0;
-
+let printRoundScore = function () {
+    results[0].textContent = playerScore;
+    results[1].textContent  = computerScore;
 }
+
 
 // Prints the winner at the end of the game.
 function printFinalResult() {
-    if (playerScore > computerScore) {
-        console.log(`Player wins by ${playerScore} to ${computerScore}`);
-    } else if (computerScore > playerScore) {
-        console.log(`You lose. Computer wins by ${computerScore} to ${playerScore}`)
-    } else {
-        console.log("Its a draw!");
+    if (playerScore === 5 || computerScore === 5) {
+        console.log("Game finished");
+        if (playerScore > computerScore) {
+            console.log(`Player wins by ${playerScore} to ${computerScore}`);
+            results[2].textContent = `Player wins by ${playerScore} to ${computerScore}`;
+        } else if (computerScore > playerScore) {
+            console.log(`You lose. Computer wins by ${computerScore} to ${playerScore}`)
+            results[2].textContent = `You lose. Computer wins by ${computerScore} to ${playerScore}`;
+        } else {
+            console.log("Its a draw!");
+        }
     }
 }
 
-game();
